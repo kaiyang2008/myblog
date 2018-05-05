839a63302390bbad2cf9911ede519cb6200fe4bb
kaiy <kai_yang2008@163.com>
2018-05-06 00:00:14 +0800

    add homepage to display data in DB

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http import HttpResponse
+
+from .models import Post
+
+def homepage(request):
+    posts = Post.objects.all()
+    post_list = list()
+    for count,post in enumerate(posts):
+        post_list.append("No.{}".format(str(count)) + str(post)+"<br>")
+    return HttpResponse(post_list)
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from mainsite.views import homepage
 
 urlpatterns = [
     # Examples:
@@ -8,4 +9,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/', 'myblog.views.hello',name='hello'),
+    url(r'^$', homepage)
 ]
